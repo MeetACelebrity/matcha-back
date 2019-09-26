69813df8b9f44a2800e78b93efcf833eb3b7ab52
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-09-26 13:36:20 +0200

    Set up files architecture

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import Routes from './routes';
+
 async function app() {
-    console.log('Welcome !');
+    const server = express();
+
+    server
+        .use(bodyParser.urlencoded({ extended: false }))
+        .use(bodyParser.json());
+
+    Routes(server);
+
+    server.listen(3000, '0.0.0.0', () => {
+        console.log('Example app listening on port 3000!');
+    });
 }
 
 app().catch(console.error);
