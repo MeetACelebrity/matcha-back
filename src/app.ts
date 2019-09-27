37cf7921176cc8080bfdbd9a9383e88c8cb5f63d
Baptiste DEVESSIER <bdevessi@student.42.fr>
2019-09-27 19:17:39 +0200

    fix(session): fix express-session necessary config

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,8 @@ async function app() {
                     httpOnly: true,
                     signed: true,
                 },
+                resave: false,
+                saveUninitialized: true,
             })
         )
         .use((req, res, next) => {
