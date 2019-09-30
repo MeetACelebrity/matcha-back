0cfa4190c0308ef165951f6b61234b76a99b6821
Remy CRESTEY <rcrestey@student.42.fr>
2019-09-30 12:30:24 +0200

    feat: config correctly nodemon and move validation outside of sign-up middleware

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request } from 'express';
 import { Validator, ValidatorObject } from '../utils/validator';
 import { createUser } from '../models/user';
 
@@ -17,20 +17,49 @@ const enum SignUpStatusCode {
     UNKNOWN_ERROR = 'UNKNOWN_ERROR',
 }
 
+const stringSchema = Validator.string()
+    .min(3)
+    .max(20);
+
+const signUpSchema: ValidatorObject = Validator.object().keys({
+    email: Validator.string().email(),
+    username: stringSchema,
+    givenName: stringSchema,
+    familyName: stringSchema,
+    password: Validator.string().min(6),
+});
+
+function signUpRouteValidation(req: Request): SignUpStatusCode {
+    const validationResult = Validator.validate(signUpSchema, req.body);
+
+    if (typeof validationResult !== 'boolean') {
+        const {
+            error: { concernedKey },
+        } = validationResult;
+
+        switch (concernedKey) {
+            case 'email':
+                return SignUpStatusCode.EMAIL_INCORRECT;
+            case 'username':
+                return SignUpStatusCode.USERNAME_INCORRECT;
+            case 'givenName':
+                return SignUpStatusCode.GIVEN_NAME_INCORRECT;
+            case 'familyName':
+                return SignUpStatusCode.FAMILY_NAME_INCORRECT;
+            case 'password':
+                return SignUpStatusCode.PASSWORD_INCORRECT;
+            default:
+                return SignUpStatusCode.UNKNOWN_ERROR;
+        }
+    }
+
+    return SignUpStatusCode.DONE;
+}
+
 export default function authRoutes(): Router {
-    const stringSchema = Validator.string()
-        .min(3)
-        .max(20);
-
-    const signUpSchema: ValidatorObject = Validator.object().keys({
-        email: Validator.string().email(),
-        username: stringSchema,
-        givenName: stringSchema,
-        familyName: stringSchema,
-        password: Validator.string().min(6),
-    });
-
     const router = Router();
+
+    router.route('/sign-in').get().get;
 
     router.get('/sign-in', (req, res) => {
         console.log('res.locals =', res.locals);
@@ -39,38 +68,11 @@ export default function authRoutes(): Router {
     });
 
     router.post('/sign-up', async (req, res) => {
-        console.log(req.body);
         try {
-            const validationResult = Validator.validate(signUpSchema, req.body);
+            const statusCode = signUpRouteValidation(req);
 
-            if (typeof validationResult !== 'boolean') {
-                const {
-                    error: { concernedKey },
-                } = validationResult;
-
+            if (statusCode !== SignUpStatusCode.DONE) {
                 res.status(400);
-
-                let statusCode: SignUpStatusCode;
-
-                switch (concernedKey) {
-                    case 'email':
-                        statusCode = SignUpStatusCode.EMAIL_INCORRECT;
-                        break;
-                    case 'username':
-                        statusCode = SignUpStatusCode.USERNAME_INCORRECT;
-                        break;
-                    case 'givenName':
-                        statusCode = SignUpStatusCode.GIVEN_NAME_INCORRECT;
-                        break;
-                    case 'familyName':
-                        statusCode = SignUpStatusCode.FAMILY_NAME_INCORRECT;
-                        break;
-                    case 'password':
-                        statusCode = SignUpStatusCode.PASSWORD_INCORRECT;
-                        break;
-                    default:
-                        statusCode = SignUpStatusCode.UNKNOWN_ERROR;
-                }
 
                 res.json({
                     statusCode,
@@ -78,6 +80,10 @@ export default function authRoutes(): Router {
 
                 return;
             }
+
+            res.json({ statusCode });
+
+            return;
 
             const result = await createUser({ db: res.locals.db, ...req.body });
             if (result === null) {
