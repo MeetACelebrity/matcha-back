b468c90a63053affe553c114d0bea030e802e4c0
Remy CRESTEY <rcrestey@student.42.fr>
2019-10-03 17:13:32 +0200

    feat(email): create email class to send emails with elegance

---
+++ src/global.d.ts
@@ -0,0 +1,15 @@
+declare module 'heml' {
+    interface Result {
+        errors: string[];
+        html: string;
+        metadata: {
+            meta: any[];
+            subject: string;
+            size: string;
+        };
+    }
+
+    function heml(text: string): Promise<Result>;
+
+    export = heml;
+}
